43bee5f63068e31d3fff5d7dbb5f44aba7b66495
Suntooth <suntooooth@gmail.com>
2025-07-28 00:13:31 +0100

    Create main program

---
+++ ytdlp.py
@@ -0,0 +1,39 @@
+import subprocess
+
+base = "yt-dlp.exe"
+
+while True:
+    url = ' "' + input("URL: ") + '"'
+    clip = ""
+    subs = ""
+
+
+    pathChoice = True if input("[S]et path or [D]on't set path? ").lower() == "s" else False
+    if pathChoice:
+        path = ' -o "' + input("Path (with title format): ") + '"'
+    else:
+        path = ' -o "Output/%(title)s - %(id)s.%(ext)s"'
+
+
+    clipChoice = True if input("[C]lip or [N]o clip? ").lower() == "c" else False
+    if clipChoice:
+        start = input("Start: ")
+        end = input("End: ")
+        clip = ' --download-sections "*' + start + '-' + end + '"'
+        
+    
+    audioChoice = True if input("[A]udio or [V]ideo? ").lower() == "a" else False
+    if audioChoice:
+        command = base + " -f ba" + path + clip + url
+        
+
+    else:
+        subsChoice = True if input("[S]ubs or [N]o subs? ").lower() == "s" else False
+        if subsChoice:
+             subs = " --write-subs --convert-subs srt"
+
+        command = base + path + clip + subs + url
+            
+    print("")
+    subprocess.run(command)
+    print("Done.\n\n")
